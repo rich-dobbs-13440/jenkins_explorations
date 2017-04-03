b37aa2c8df0dbbc63e635280eb30d0048a5224f9
rich-dobbs-13440 <rich.dobbs.13440@gmail.com>
2017-04-02 19:46:26 -0600

    Try to add a step name?

--- pipelines/simpleBuildWrapperExample.groovy
+++ pipelines/simpleBuildWrapperExample.groovy
@@ -3,7 +3,7 @@ node {
     // This is the current syntax for invoking a build wrapper, naming the class.
     wrap([$class: 'AnsiColorBuildWrapper']) {
         // Just some echoes to show the ANSI color.
-        stage {
+        stage("Just a stage"){
         	"\u001B[31mI'm Red\u001B[0m Now not"
         }
     }
